3a2239dd2314294a3d86da94368be1b78cd20c62
alsonentuna <alson.entuna@outlook.com>
2024-01-29 19:54:07 +0100

    + caution definitions for dynamic linking

---
+++ src/CautionEngine/CautionDefinitions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Compiler specific macros...
+#if defined (_MSC_VER)
+	#ifdef CAUTION_SHARED
+		#define CAUTION_API __declspec(dllexport)
+	#else
+		#define CAUTION_API __declspec(dllimport)
+	#endif
+#endif
+
+#define CAT_DEPRECATED(message) __declspec(deprecated(message))
