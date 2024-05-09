ec0bf2783b51c7aabc85e3fe9bdfe5ed8e3231ba
alsonentuna <alson.entuna@outlook.com>
2024-05-09 15:54:18 +0200

    * adding caution common platform

---
+++ src/CautionCommon/Platform/CautionPlatform.h
@@ -0,0 +1,7 @@
+#include <memory>
+
+#define DECLARE_SHARED_POINTERS(name)                   \
+  typedef std::shared_ptr<name> name ##       SharedPtr;      \
+  typedef std::shared_ptr<const name> name ## ConstSharedPtr; \
+  typedef std::weak_ptr<name> name ##         WeakPtr;  \
+  typedef std::weak_ptr<const name> name ##   ConstWeakPtr;
