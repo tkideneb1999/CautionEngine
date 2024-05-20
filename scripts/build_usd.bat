92c68a31b4a3601eefb2c1320ad698c9f3a9a1ae
alsonentuna <alson.entuna@outlook.com>
2024-05-20 18:43:59 +0200

    ! changes to the build script
    (cherry picked from commit 3a0e6ee4229844c5733b839a95e6c3a8d7db0942)

--- scripts/build_usd.bat
+++ scripts/build_usd.bat
@@ -2,7 +2,6 @@
 echo "Building USD Library"
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat"
-python "validate_python_and_packages.py"
 python "..\thirdparty\usd\build_scripts\build_usd.py" ..\thirdparty\usd\bin
 python "ensure_usd_paths.py"
 pause
